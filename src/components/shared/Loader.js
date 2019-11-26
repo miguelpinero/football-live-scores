39fa9abe545f226af8e875033e813bbcf85a4384
Miguel Pinero <miguelarmanp@gmail.com>
2019-11-26 01:17:23 -0500

    Fix loader path

--- src/components/shared/Loader.js
+++ src/components/shared/Loader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import loader from "../../../Alternate-Preloader.gif";
+import loader from "../../Alternate-Preloader.gif";
 
 export default function Loader() {
   return <img className="Loader" src={loader} alt="Loader" />;
