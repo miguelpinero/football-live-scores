37a0e6ba076aadd2ebe3563abe5f60362244bf96
Miguel Pinero <miguelarmanp@gmail.com>
2019-11-26 23:08:00 -0500

    Updates

---
+++ precache-manifest.b270edd64eceec661727acd9c0a2452c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1b5ee5859fb7383be3a5e32e1f7234",
+    "url": "/football-live-scores/index.html"
+  },
+  {
+    "revision": "9e96097e9d029aaa4d53",
+    "url": "/football-live-scores/static/css/2.90172c5a.chunk.css"
+  },
+  {
+    "revision": "72c5a66ead786793eddb",
+    "url": "/football-live-scores/static/css/main.abd21947.chunk.css"
+  },
+  {
+    "revision": "9e96097e9d029aaa4d53",
+    "url": "/football-live-scores/static/js/2.361ad6e5.chunk.js"
+  },
+  {
+    "revision": "72c5a66ead786793eddb",
+    "url": "/football-live-scores/static/js/main.925e9a59.chunk.js"
+  },
+  {
+    "revision": "120385f5521c4ee67bfc",
+    "url": "/football-live-scores/static/js/runtime-main.ed13fda1.js"
+  }
+]);
