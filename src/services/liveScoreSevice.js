71fe7998014d4c6f83837613aed7ac764253d5e6
Miguel Pinero <miguelarmanp@gmail.com>
2019-11-27 08:39:54 -0500

    Prepare app to use the ./build

--- src/services/liveScoreSevice.js
+++ src/services/liveScoreSevice.js
@@ -1,19 +1,21 @@
 import { upcomingMatchesParser, matchesParser } from "../lib/matchesParser";
 
+const BASE_URL = "http://localhost:4000";
+
 export function fetchPastMatches() {
-  return fetch("/past")
+  return fetch(`${BASE_URL}/past`)
     .then(response => response.json())
     .then(data => matchesParser(data));
 }
 
 export function fetchLiveMatches() {
-  return fetch("/live")
+  return fetch(`${BASE_URL}/live`)
     .then(response => response.json())
     .then(data => matchesParser(data));
 }
 
 export function fetchUpcomingMatches() {
-  return fetch("/upcoming")
+  return fetch(`${BASE_URL}/upcoming`)
     .then(response => response.json())
     .then(data => upcomingMatchesParser(data));
 }
