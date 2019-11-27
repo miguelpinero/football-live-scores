a801495e97fff02974f954f94f448f0dbf1a2a86
Miguel Pinero <miguelarmanp@gmail.com>
2019-11-27 08:38:07 -0500

    Abstract implmentation of consume the data in a Ruby server

--- src/services/liveScoreSevice.js
+++ src/services/liveScoreSevice.js
@@ -1,41 +1,19 @@
-import { buildURL } from "../lib/buildURL";
 import { upcomingMatchesParser, matchesParser } from "../lib/matchesParser";
 
-const FROM = "2019-11-01";
-const TO = "2019-11-27";
-const COMPETITION_ID = "244";
-const PAST_MATCHES_URL = "/api-client/scores/history.json";
-const UPCOMING_MATCHES_URL = "/api-client/fixtures/matches.json";
-const LIVE_MATCHES_URL = "/api-client/scores/live.json";
-
-export function fetchPastMatches(from = FROM, to = TO) {
-  const url = buildURL(PAST_MATCHES_URL, {
-    from,
-    to,
-    competition_id: COMPETITION_ID
-  });
-
-  return fetch(url)
+export function fetchPastMatches() {
+  return fetch("/past")
     .then(response => response.json())
     .then(data => matchesParser(data));
 }
 
 export function fetchLiveMatches() {
-  const url = buildURL(LIVE_MATCHES_URL, {
-    competition_id: COMPETITION_ID
-  });
-
-  return fetch(url)
+  return fetch("/live")
     .then(response => response.json())
     .then(data => matchesParser(data));
 }
 
 export function fetchUpcomingMatches() {
-  const url = buildURL(UPCOMING_MATCHES_URL, {
-    competition_id: COMPETITION_ID
-  });
-
-  return fetch(url)
+  return fetch("/upcoming")
     .then(response => response.json())
     .then(data => upcomingMatchesParser(data));
 }
