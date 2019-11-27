759f85c2985f3514eb86b9ad1fe6ac64c95d7cf6
Miguel Pinero <miguelarmanp@gmail.com>
2019-11-27 10:14:50 -0500

    Add button to show push notification

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -3,7 +3,7 @@ import PastMatches from "components/pages/PastMatches";
 import UpcomingMatches from "components/pages/UpcomingMatches";
 import LiveMatches from "components/pages/LiveMatches";
 import { Switch, Route, Link, useLocation } from "react-router-dom";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Button } from "antd";
 import "./Home.css";
 
 const { Item } = Menu;
@@ -17,6 +17,18 @@ const menuOrder = {
 export default function Home() {
   let location = useLocation();
   const selectedKey = menuOrder[location.pathname] || "1";
+
+  function showNotification() {
+    Notification.requestPermission(function(result) {
+      if (result === "granted") {
+        navigator.serviceWorker.ready.then(function(registration) {
+          registration.showNotification("Mi custom push notification", {
+            body: "This is my first Push Notification "
+          });
+        });
+      }
+    });
+  }
 
   return (
     <div className="App">
@@ -34,6 +46,9 @@ export default function Home() {
             </Item>
           </Menu>
         </Header>
+        <Button type="primary" key="console" onClick={showNotification}>
+          Show Push Notification
+        </Button>
         <Switch>
           <Route exact path="/">
             <PastMatches />
