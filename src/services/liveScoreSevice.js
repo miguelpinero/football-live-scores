8233f105440119503bf37b8b695e777a8dab9a8c
Miguel Pinero <miguelarmanp@gmail.com>
2019-11-27 08:36:08 -0500

    Finish implementation of PWA

--- src/services/liveScoreSevice.js
+++ src/services/liveScoreSevice.js
@@ -1,41 +1,21 @@
-import { buildURL } from "../lib/buildURL";
 import { upcomingMatchesParser, matchesParser } from "../lib/matchesParser";
 
-const FROM = "2019-11-01";
-const TO = "2019-11-27";
-const COMPETITION_ID = "244";
-const PAST_MATCHES_URL = "/api-client/scores/history.json";
-const UPCOMING_MATCHES_URL = "/api-client/fixtures/matches.json";
-const LIVE_MATCHES_URL = "/api-client/scores/live.json";
+const BASE_URL = "http://localhost:4000";
 
-export function fetchPastMatches(from = FROM, to = TO) {
-  const url = buildURL(PAST_MATCHES_URL, {
-    from,
-    to,
-    competition_id: COMPETITION_ID
-  });
-
-  return fetch(url)
+export function fetchPastMatches() {
+  return fetch(`${BASE_URL}/past`)
     .then(response => response.json())
     .then(data => matchesParser(data));
 }
 
 export function fetchLiveMatches() {
-  const url = buildURL(LIVE_MATCHES_URL, {
-    competition_id: COMPETITION_ID
-  });
-
-  return fetch(url)
+  return fetch(`${BASE_URL}/live`)
     .then(response => response.json())
     .then(data => matchesParser(data));
 }
 
 export function fetchUpcomingMatches() {
-  const url = buildURL(UPCOMING_MATCHES_URL, {
-    competition_id: COMPETITION_ID
-  });
-
-  return fetch(url)
+  return fetch(`${BASE_URL}/upcoming`)
     .then(response => response.json())
     .then(data => upcomingMatchesParser(data));
 }
